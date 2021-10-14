aba0391b0b3fcbdd2ef05b60e5c0ed20720bb883
Mervin <mengqing723@gmail.com>
2021-10-14 18:18:54 +0800

    feat: 增加deepClone/防抖debounce/节流throttle函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -135,90 +135,145 @@ export function param2Obj(url) {
 }
 
 /**
- * Merges two objects, giving the last one precedence
- * @param {Object} target
- * @param {(Object|Array)} source
- * @returns {Object}
- */
-export function objectMerge(target, source) {
-  if (typeof target !== 'object') {
-    target = {}
-  }
-  if (Array.isArray(source)) {
-    return source.slice()
-  }
-  Object.keys(source).forEach(property => {
-    const sourceProperty = source[property]
-    if (typeof sourceProperty === 'object') {
-      target[property] = objectMerge(target[property], sourceProperty)
-    } else {
-      target[property] = sourceProperty
-    }
-  })
-  return target
-}
-
-/**
+ * 函数防抖
  * @param {Function} func
  * @param {number} wait
  * @param {boolean} immediate
  * @return {*}
  */
 export function debounce(func, wait, immediate) {
-  let timeout, args, context, timestamp, result
+  let timeout, previous, args, result, context
 
   const later = function() {
     // 据上一次触发时间间隔
-    const last = +new Date() - timestamp
-
-    // 上次被包装函数被调用时间间隔 last 小于设定时间间隔 wait
-    if (last < wait && last > 0) {
-      timeout = setTimeout(later, wait - last)
+    const passed = Date.now() - previous
+    // 上次触发时间间隔 小于设定时间间隔
+    if (wait > passed) {
+      timeout = setTimeout(later, wait - passed)
     } else {
       timeout = null
-      // 如果设定为immediate===true，因为开始边界已经调用过了此处无需调用
-      if (!immediate) {
-        result = func.apply(context, args)
-        if (!timeout) context = args = null
-      }
+      if (!immediate) result = func.apply(context, args)
+      // This check is needed because `func` can recursively invoke `debounced`.
+      if (!timeout) args = context = null
     }
-  }
+  };
 
-  return function(...args) {
+  const debounced = function() {
+    args = arguments
     context = this
-    timestamp = +new Date()
-    const callNow = immediate && !timeout
-    // 如果延时不存在，重新设定延时
-    if (!timeout) timeout = setTimeout(later, wait)
-    if (callNow) {
-      result = func.apply(context, args)
-      context = args = null
+    previous = Date.now()
+    if (!timeout) {
+      // 如果延时不存在，重新设定延时
+      timeout = setTimeout(later, wait);
+      if (immediate) result = func.apply(context, args)
     }
-
     return result
   }
+
+  debounced.cancel = function() {
+    clearTimeout(timeout)
+    timeout = args = context = null
+  }
+
+  return debounced
 }
 
 /**
- * This is just a simple version of deep copy
- * Has a lot of edge cases bug
- * If you want to use a perfect deep copy, use lodash's _.cloneDeep
- * @param {Object} source
- * @returns {Object}
+ * 函数节流
+ * @param {*} func 
+ * @param {*} wait 
+ * @param {*} options 
+ * @returns 
+ */
+export function throttle(func, wait, options) {
+  let timeout, context, args, result
+  // 标记时间戳
+  let previous = 0
+  // options可选属性 leading: true/false 表示第一次事件马上触发回调/等待wait时间后触发
+  // options可选属性 trailing: true/false 表示最后一次回调触发/最后一次回调不触发
+  if (!options) options = {}
+
+  const later = function() {
+    previous = options.leading === false ? 0 : Date.now()
+    timeout = null
+    result = func.apply(context, args)
+    if (!timeout) context = args = null
+  }
+
+  const throttled = function() {
+    const _now = Date.now()
+    // 如果是第一次触发且选项设置不立即执行回调
+    if (!previous && options.leading === false) previous = _now
+    // 距离下次触发回调还需等待的时间
+    const remaining = wait - (_now - previous)
+    context = this
+    args = arguments
+    // 等待时间 <= 0或者不科学地 > wait（异常情况）
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = _now
+      // 触发回调
+      result = func.apply(context, args)
+      if (!timeout) context = args = null
+    } else if (!timeout && options.trailing !== false) {
+      // 在定时器不存在且选项设置最后一次触发需要执行回调的情况下
+      // 设置定时器，间隔remaining时间后执行later
+      timeout = setTimeout(later, remaining)
+    }
+    return result
+  }
+
+  throttled.cancel = function() {
+    clearTimeout(timeout)
+    previous = 0
+    timeout = context = args = null
+  }
+
+  return throttled
+}
+
+/**
+ * 
+ * @param {any} source 
+ * @returns 
  */
 export function deepClone(source) {
-  if (!source && typeof source !== 'object') {
-    throw new Error('error arguments', 'deepClone')
+  let copy
+  
+  // eslint-disable-next-line yoda
+  if (null === source || 'object' !== typeof source) return source
+
+  // Handle Date
+  if (source instanceof Date) {
+    copy = new Date()
+    copy.setTime(source.getTime())
+    return copy
   }
-  const targetObj = source.constructor === Array ? [] : {}
-  Object.keys(source).forEach(keys => {
-    if (source[keys] && typeof source[keys] === 'object') {
-      targetObj[keys] = deepClone(source[keys])
-    } else {
-      targetObj[keys] = source[keys]
+
+  // Handle Array
+  if (source instanceof Array) {
+    copy = []
+    for (let i = 0, len = source.length; i < len; i++) {
+      copy[i] = deepClone(source[i])
     }
-  })
-  return targetObj
+    return copy
+  }
+
+  // Handle Object
+  if (source instanceof Object) {
+    copy = {}
+    for (const attr in source) {
+      if (Object.prototype.hasOwnProperty.call(source, attr)) {
+        copy[attr] = deepClone(source[attr])
+      }
+    }
+    return copy
+  }
+
+  throw new Error("Unable to copy! Its type isn't supported.")
 }
 
 /**

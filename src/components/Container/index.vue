5175b7f314a13a7c08e92b473c86916b7edc00da
Mervin <mengqing723@gmail.com>
2021-12-14 18:10:13 +0800

    style: 更改layout样式

--- src/components/Container/index.vue
+++ src/components/Container/index.vue
@@ -3,12 +3,10 @@
   <div v-if="$slots.header" :class="['ve-container-head', headClass]" :style="headStyle">
     <slot name="header" />
   </div>
-  <div
-    :class="['ve-container-main', bodyClass]"
-    :style="bodyStyle">
+  <div :class="['ve-container-main', bodyClass]" :style="bodyStyle">
     <slot />
   </div>
-  <div v-if="$slots.footer" :class="['ve-container-foot', footClass]" :style="bodyStyle">
+  <div v-if="$slots.footer" :class="['ve-container-foot', footClass]" :style="footStyle">
     <slot name="footer" />
   </div>
 </ve-flex>
@@ -53,11 +51,15 @@ export default {
   }
   &-head, &-foot{
     position: relative;
+    z-index: 1;
+  }
+  &-head{
+    box-shadow: 2px 0 12px rgba(0, 0, 0, .1);
   }
   &-foot{
     padding: 16px 20px;
     background-color: #fff;
-    box-shadow: 0 -6px 16px -8px rgba($color: #000000, $alpha: 0.08);
+    box-shadow: 0 -6px 16px -8px rgba($color: #000000, $alpha: 0.1);
   }
 }
 </style>

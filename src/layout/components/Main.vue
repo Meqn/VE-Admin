d2083537336d369489c0aa0377761bebfb023e7e
Mervin <mengqing723@gmail.com>
2022-03-18 12:05:26 +0800

    fix: 更改router缓存,待优化

--- src/layout/components/Main.vue
+++ src/layout/components/Main.vue
@@ -9,6 +9,18 @@
 </template>
 
 <script>
+function queryCachedViews(routes, result = []) {
+  routes.forEach(route => {
+    if (route.meta?.cache) {
+      result.push(route.name)
+    }
+    if (route.children) {
+      queryCachedViews(route.children, result)
+    }
+  })
+  return result
+}
+
 export default {
   name: 'LayoutMain',
   inject: {
@@ -17,8 +29,7 @@ export default {
   computed: {
     cachedViews() {
       // 需要缓存的 嵌套路由的父级组件必须是 `BlankView`, 否则需要将父组件 name追加到此处
-      return ['BlankView', 'TableList']
-      // return this.$store.getters?.cachedViews || []
+      return [].concat(queryCachedViews(this.top.routes))
     },
     key() {
       return this.$route.path
